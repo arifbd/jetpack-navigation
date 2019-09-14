e8aa46fe2024499ff3c3b91e2ce789a5609fc2f2
Md Arif <arif991846@gmail.com>
2019-09-14 23:07:07 +0600

    navigation implemented with bottom navigation and navigation drawer, navigation ui, safe args etc

---
+++ README.md
@@ -0,0 +1,25 @@
+# Jetpack - Navigation
+
+A Project that help to learn Navigation.
+
+## Getting Started
+
+Navigation refers to the interactions that allow users to navigate across, into, and back out from the different pieces of content within your app. Android Jetpack's Navigation component helps you implement navigation, from simple button clicks to more complex patterns, such as app bars and the navigation drawer.
+
+### Prerequisites
+
+To use Navigation with Android Studio, you must use Android Studio 3.3 or higher.
+
+
+## Language Used
+
+* [Kotlin](https://kotlinlang.org/) - Android project with Kotlin
+
+## Credit
+
+* **ResoCoder** - [link](https://github.com/ResoCoder/navigation-component-tutorial)
+* **William Reed** - [link](https://stackoverflow.com/a/51974492)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details
---
+++ app/src/main/java/com/ennoblesoft/arif/navigationstudy/CameraFragment.kt
@@ -0,0 +1,29 @@
+package com.ennoblesoft.arif.navigationstudy
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_camera.*
+import java.util.*
+
+class CameraFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_camera, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        button_photos.setOnClickListener {
+            val random = Random()
+
+            val nextAction = CameraFragmentDirections.nextAction(random.nextInt(100))
+            Navigation.findNavController(it).navigate(nextAction)
+        }
+    }
+}
